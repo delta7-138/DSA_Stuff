dc12a8776c657e96b3816b26b8d5c077a8acb2ca
Krishna Sharma <53052253+delta7-138@users.noreply.github.com>
2020-01-21 15:14:29 +0530

    Add files via upload

--- Longest_Common_Substring.c
+++ Longest_Common_Substring.c
@@ -27,6 +27,7 @@ void traverse_list(struct Node *head){
 int check_substring(char *str_1 , char *str_2 ,struct Node *hash_table[] ,  int len){
     //struct Node *hash_table[LARGE] = {NULL};
     int str_1_len = strlen(str_1);
+    int str_2_len = strlen(str_2);
     int hash_value = str_1[0] - '0' , x = 2;
     for(int i = 0; i<len ; i++){
       if(hash_value<0){
@@ -52,12 +53,40 @@ int check_substring(char *str_1 , char *str_2 ,struct Node *hash_table[] ,  int 
       sub_str_hash = (sub_str_hash * 2 + str_2[j]-'0')%PRIME;
       z = (z * 2)%PRIME;
     }
-    while(hash_table[sub_str_hash]!=NULL){
+    struct Node *temp = hash_table[sub_str_hash];
+    while(temp!=NULL){
       int m;
       for(m = 0; m<len; m++){
-        
+        int j = temp->data;
+        for(; j<temp->data + len; j++){
+          if(str_2[m]!=str_1[j])
+            break;
+        }
+        if(j==temp->data + len)
+           return 1;
+        }
+        temp = temp->next;
+    }
+    //rolling hash
+
+    for(int k = 1; k<=str_2_len-len; k++){
+      sub_str_hash = (sub_str_hash * 2 + str_2[k + len -1] - '0' + (str_2[k-1] - '0') * z)%PRIME;
+
+      struct Node *temp = hash_table[sub_str_hash];
+      while(temp!=NULL){
+        for(int l = 0; l<len; l++){
+          int p = temp->data;
+          for(p; p<temp->data + len; p++){
+            if(str_2[l]!=str_1[p])
+              break;
+          }
+          if(p==temp->data + len)
+            return 1;
+        }
+        temp = temp->next;
       }
     }
+    return 0;
 }
 int main(){
   char str_1[LARGE];
@@ -71,14 +100,13 @@ int main(){
 
   //HASH TABLE PART
   struct Node *hash_table[43];
-  check_substring(str_1 , str_2 , hash_table , 3);
+  int i = check_substring(str_1 , str_2 , hash_table , 3);
   //for(int i = 0; i<PRIME; i++){
     //if(hash_table[i]!=NULL){
       //traverse_list(hash_table[i]);
     //}
   //}
-
-
+  printf("%d\n" , i);
   return 0;
 
 }

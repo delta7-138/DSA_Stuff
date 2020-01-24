dc8830dd33197cc1254c6ecc000983fd85ccef4d
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-01-24 20:30:59 +0000

    commit

---
+++ Floyd_Cycling_Algo.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Node *push(struct Node **head , int data){
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = *head;
+    *head = temp;
+    return temp;
+}
+int check_straight_or_not(struct Node *head){
+    struct Node *p1 = head;
+    struct Node *p2 = head;
+
+    while(!p2){
+        p1 = p1->next;
+        if(p2!=NULL && p2->next!=NULL){
+            p2 = (p2->next)->next;
+        }
+        if(p2==NULL){
+            return 0;
+        }else if(p1==p2){
+            return 1;
+        }
+    }
+}
+int main(){
+    int num_str, inp;
+    scanf("%d" , &num_str);
+
+    int num_curve;
+    scanf("%d" , &num_curve);
+
+    //Creation of straight linked list 
+    struct Node *str_head = NULL;
+    for(int i = 0; i<num_str; i++){
+        scanf("%d"  , &inp);
+        push(&str_head , inp);
+    }
+
+    //Creation of circular linked list
+    struct Node *curve_head = NULL;
+    for(int i = 0; i<num_curve; i++){
+        scanf("%d" , &inp);
+        push(&curve_head , inp);
+    }
+
+    struct Node *temp = curve_head;
+    for(int i = 0; i<4; i++){
+        temp = temp->next;
+    }
+
+    struct Node *temp_2 = curve_head;
+    while(temp_2->next!=NULL){
+        temp_2 = temp_2->next;
+    }
+    temp_2->next = temp;
+
+    int ans_1 = check_straight_or_not(str_head);
+    int ans_2 = check_straight_or_not(curve_head);
+
+    return 0;
+
+}
--- merge_sort.c
+++ merge_sort.c
@@ -1,16 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LARGE 100000
+int arr_ans[LARGE];
+void merge(int arr[] , int l , int u, int mid){
+    int start = l , end = mid + 1, p = 0;
+    while(start<=mid && end<=u){
+        if(arr[start]<=arr[end]){
+            arr_ans[p] = arr[start];
+            start++; 
+            p++;
+        }else{
+            arr_ans[p] = arr[end];
+            end++;
+            p++;
+        }
+    }
+
+    while(start<=mid){
+        arr_ans[p] = arr[start];
+        start++;
+        p++;
+    }
+    while(end<=u){
+        arr_ans[p] = arr[end];
+        end++;
+        p++;
+    }
+}
+int merge_sort(int arr[] , int l , int u){
+    if(l<u){
+        int mid = (l + u)/2;
+
+        merge_sort(arr , l , mid);
+        merge_sort(arr , mid + 1 , u);
+
+        merge(arr , l , u , mid);
+    }
+}
 int main(){
     int num_inp;
     scanf("%d" , &num_inp);
 
     int arr_inp[LARGE];
-    int arr_ans[LARGE];
 
     for(int i = 0; i<num_inp; i++){
         scanf("%d" , &arr_inp[i]);
     }
 
+    for(int i = 0; i<num_inp; i++){
+        printf("%d\n" , arr_ans[i]);
+    }
     return 0;
 }

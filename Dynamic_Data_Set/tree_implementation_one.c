1b38700e63fa04c4dd8e00a3b592c61aef5199d1
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-02-16 05:08:49 +0000

    tree

---
+++ Dynamic_Data_Set/tree_implementation_one.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *par;
+    struct Node *lc;
+    struct Node *rc;
+    int level;
+    int height;
+};
+int main(){
+    
+}

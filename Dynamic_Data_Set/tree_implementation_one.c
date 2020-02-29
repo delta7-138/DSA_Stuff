d63863dca63aa235fa89525ce1a913de5584641b
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-02-29 16:37:35 +0000

    recursive tree functions  done

--- Dynamic_Data_Set/tree_implementation_one.c
+++ Dynamic_Data_Set/tree_implementation_one.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LARGE 1000
+
+//Only for CBT plus uses recursive calling 
 struct Node{
     int data;
     struct Node *par;
@@ -10,17 +12,45 @@ struct Node{
     int height;
 };
 
+//Stack Functions 
+int pop(int stack[] , int *top){
+    *top = *top - 1;
+    return stack[*top + 1];
+}
+
+int push(int stack[] , int *top , int x){
+    stack[++(*top)] = x;
+}
+
+void traverse_stack(int stack[] , int top){
+    for(int i = 0; i<=top; i++){
+        printf("%d\n" , stack[i]);
+    }
+}
+
 //Tree Functions
-void preorder_traversal(struct Node *tree[]){
-
+void preorder_traversal(struct Node *node){
+    if(node){
+        printf("%d " , node->data); 
+        preorder_traversal(node->lc);
+        preorder_traversal(node->rc);
+    }//printf("\n");
 }
 
-void postorder_traversal(struct Node *tree[]){
-
+void postorder_traversal(struct Node *node){
+    if(node){
+        postorder_traversal(node->lc);
+        postorder_traversal(node->rc);
+        printf("%d " , node->data);
+    }//printf("\n");
 }
 
-void inorder_traversal(struct Node *tree[]){
-
+void inorder_traversal(struct Node *node){
+    if(node){
+        inorder_traversal(node->lc);
+        printf("%d " , node->data);
+        inorder_traversal(node->rc);
+    }//printf("\n");
 }
 
 int main(){
@@ -42,6 +72,14 @@ int main(){
         tree[i]->lc = tree[2 * i + 1];
         tree[i]->rc = tree[2 * i + 2];
     }
-
+    printf("POST\n");
+    postorder_traversal(tree[0]);
+    printf("\n");
+    printf("PRE\n");
+    preorder_traversal(tree[0]);
+    printf("\n");
+    printf("IN\n");
+    inorder_traversal(tree[0]);
+    printf("\n");
     return 0;
 }   

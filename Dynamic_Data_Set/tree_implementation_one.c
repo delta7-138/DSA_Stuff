88e782fe1ae2b5b9f7f450900cd2aa7c517ea7a4
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-02-29 13:39:11 +0000

    tree implementation done

--- Dynamic_Data_Set/tree_implementation_one.c
+++ Dynamic_Data_Set/tree_implementation_one.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define LARGE 1000
 struct Node{
     int data;
     struct Node *par;
@@ -8,6 +9,39 @@ struct Node{
     int level;
     int height;
 };
-int main(){
-    
+
+//Tree Functions
+void preorder_traversal(struct Node *tree[]){
+
 }
+
+void postorder_traversal(struct Node *tree[]){
+
+}
+
+void inorder_traversal(struct Node *tree[]){
+
+}
+
+int main(){
+    struct Node *tree[LARGE]; 
+    int n;
+    scanf("%d" , &n);
+
+    for(int i = 0; i<n; i++){
+        tree[i] = (struct Node *)malloc(sizeof(struct Node));
+        scanf("%d" , &(tree[i]->data));
+    }
+
+    for(int i = 0; i<n; i++){
+        if(i!=0){
+            tree[i]->par = tree[(i-1)/2];
+        }else{
+            tree[i]->par = NULL;
+        }
+        tree[i]->lc = tree[2 * i + 1];
+        tree[i]->rc = tree[2 * i + 2];
+    }
+
+    return 0;
+}   

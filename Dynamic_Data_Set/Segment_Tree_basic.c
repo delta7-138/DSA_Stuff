f3273444bf265f543cd881211b415418dce339ba
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-03-10 08:23:03 +0000

    update done

--- Dynamic_Data_Set/Segment_Tree_basic.c
+++ Dynamic_Data_Set/Segment_Tree_basic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LARGE 10000
-int build_tree(int segment_tree[] , int arr[] , int n){
+void build_tree(int segment_tree[] , int arr[] , int n){
     for(int i = 0; i<n; i++){
         segment_tree[n + i - 1] = i;
     }
@@ -12,6 +12,26 @@ int build_tree(int segment_tree[] , int arr[] , int n){
             segment_tree[i] = segment_tree[2 * i + 2];
         }else{
             segment_tree[i] = segment_tree[2 * i + 1];
+        }
+    }
+}
+
+void update_node(int segment_tree[] , int arr[] , int i ,  int x , int n){
+    arr[i] = x;
+
+    i = (n-1 + i - 1)/2;
+
+    while(i>=0){
+        if(arr[segment_tree[2 * i + 1]]>arr[segment_tree[2 * i + 2]]){
+            segment_tree[i] = segment_tree[2 * i + 2];
+        }else{
+            segment_tree[i] = segment_tree[2 * i + 1];
+        }
+        //printf("%d\n" , i);
+
+        i = (i-1)/2;
+        if(i<1){
+            break;
         }
     }
 }
@@ -28,6 +48,12 @@ int main(){
     
     int seg_tree[LARGE * 2 + 1] = {0};
     build_tree(seg_tree , arr_inp , n);
+
+    for(int i = 0; i<2 * n-1; i++){
+        printf("%d " , seg_tree[i]);
+    }printf("\n");
+
+    update_node(seg_tree , arr_inp , 6 , 14 , n);
 
     for(int i = 0; i<2 * n-1; i++){
         printf("%d " , seg_tree[i]);

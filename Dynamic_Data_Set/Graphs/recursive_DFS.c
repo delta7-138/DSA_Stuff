3c6cc6b277f4157d5badab66fee9de6ba4538ae1
delta7-138 <krishna.sharma@iiitb.org>
2020-05-21 18:37:44 +0530

    Dijkstra done

--- Dynamic_Data_Set/Graphs/recursive_DFS.c
+++ Dynamic_Data_Set/Graphs/recursive_DFS.c
@@ -18,23 +18,20 @@ void traverse_stack(int stack[] , int top){
     }
 }
 
-int stack[LARGE];
-int top_stack = -1, count = 0;
-
 //ad-list struct
 struct listnode{
     int i;
     float weight;
     struct listnode *next;
-}
+};
 
 struct listnode *newedge(struct listnode **head , int i , float weight){
     struct listnode *temp = (struct listnode *)malloc(sizeof(struct listnode));
     temp->i = i;
     temp->weight = weight;
 
-    temp->next = head;
-    *head = *temp;
+    temp->next = *head;
+    *head = temp;
 
     return temp;
 }
@@ -46,6 +43,62 @@ void DFSvisit(struct listnode *list[] , int D[] , int F[] , int V[] , int phi[] 
         V[i] = 0;
         phi[i] = -2;
     }
-    count = 1;
+    int stack[LARGE];
+    int top_stack = -1;
+
+    int count = 1;
+    for(int i = 0; i<n; i++){
+        //traverse_stack(stack , top_stack);
+        if(V[i]==0){
+            push(stack , &top_stack , i);
+            phi[i] = -1;
+
+            while(top_stack>=0){
+                int u = pop(stack , &top_stack);
+                if(V[u]==0){
+                    V[u] = 1;
+                    D[u] = count++;
+                    push(stack , &top_stack , u);
+
+                    struct listnode *temp = list[u];
+                    while(temp!=NULL){
+                        if(V[temp->i]==0){
+                            push(stack , &top_stack , temp->i);
+                            phi[temp->i] = u; 
+                            temp = temp->next; 
+                        }
+                    }
+                }
+                else if(F[u]==0){
+                    F[u] = count++;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    int m, n;
+    printf("Enter the number of edges : ");
+    scanf("%d" , &m);
+    printf("Enter the number of nodes : ");
+    scanf("%d" , &n);
+
+    int i , j;
+    float w;
+    int D[LARGE] , F[LARGE] , V[LARGE] , phi[LARGE];
+    struct listnode *list[LARGE];
+    printf("Enter the edges in the form (i , j): \n");
     
+    for(int k = 0; k<m; k++){
+        scanf("%d %d" , &i , &j);
+        scanf("%f" , &w);
+        newedge(&list[k] , j , w);
+    }
+
+    DFSvisit(list , D , F , V , phi , n);
+    for(int i = 0; i<n; i++){
+        printf("%d %d %d %d\n" , i , D[i] , F[i] , phi[i]);
+    }
+    return 0;
 }
